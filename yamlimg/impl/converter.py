b7202195312e053babda4716b026f4f51fe0b97e
vcokltfre <vcokltfre@gmail.com>
2022-03-13 10:34:12 +0000

    feat: make dumping faster

--- yamlimg/impl/converter.py
+++ yamlimg/impl/converter.py
@@ -1,37 +1,27 @@
 from __future__ import annotations
 
 from PIL import Image
-from yaml import SafeLoader, dump, load
+from yaml import SafeLoader, load
 
 from .spec import YAMLImg
 
 
 def image_to_yaml(img: Image.Image) -> str:
-    data: dict[str, int | list[dict[str, int]]] = {
-        "version": 1,
-        "width": img.width,
-        "height": img.height,
-    }
+    data = f"version: 1\nwidth: {img.width}\nheight: {img.height}\ndata:\n"
 
-    pixels: list[dict[str, int]] = []
+    pixels: list[str] = []
 
     for x in range(img.width):
         for y in range(img.height):
             pixel: tuple[int, int, int, int] = img.getpixel((x, y))  # type: ignore
             pixels.append(
-                {
-                    "x": x,
-                    "y": y,
-                    "r": pixel[0],
-                    "g": pixel[1],
-                    "b": pixel[2],
-                    "a": pixel[3] if len(pixel) > 3 else 255,
-                }
+                "- x: {x}\n  y: {y}\n  r: {r}\n  g: {g}\n  b: {b}\n  a: {a}\n".format(
+                    x=x, y=y, r=pixel[0], g=pixel[1], b=pixel[2], a=(pixel[3] if len(pixel) == 4 else 255)
+                )
             )
 
-    data["data"] = pixels
-
-    return dump(data, default_flow_style=False)
+    data += "".join(pixels)
+    return data
 
 
 def yaml_to_image(yaml: str) -> Image.Image:
